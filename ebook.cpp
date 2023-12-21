b2c2a5eb3a4c24a34fe60b0f8ba21dab94c3325d
KruchininEN <kruchinin.e.n@yandex.ru>
2023-12-22 02:01:23 +0300

    Update ebook.cpp

--- ebook.cpp
+++ ebook.cpp
@@ -2,8 +2,8 @@
 #include <iomanip>
 #include <vector>
 
-int MAX_ID = 100000;                                                                                    // max user id
-int MAX_PAGE = 1000;                                                                                    // max page  
+int MAX_ID = 100000;    // max user id
+int MAX_PAGE = 1000;    // max page  
 
 class BookManager {
 public:
@@ -12,53 +12,63 @@ public:
         , pages_to_read_count_(MAX_PAGE, 0)
     {}
 
-    void Proceed() {                                                                                    // proceeding commands
+    // proceeding commands
+    void Proceed(std::istream& input) {
         int command_count;
-        std::cin >> command_count;
+        input >> command_count;
 
         for (size_t i = 0; i < command_count; ++i) {
             std::string command;
             std::cin >> command;
 
-            if (command == "READ") { Read(); }
-            if (command == "CHEER") { Cheer(); }
+            if (command == "READ") { Read(input); }
+            if (command == "CHEER") { Cheer(input); }
         }
     }
 
-    void Read() {
+    void Read(std::istream& input) {
         int id, page;
-        std::cin >> id >> page;
-        for (int i = id_to_read_progress_[id] + 1; i <= page; ++i) { ++pages_to_read_count_[i]; }       // changing the status of previous pages
-        id_to_read_progress_[id] = page;                                                                // recording the progress of a given user
+        input >> id >> page;
+        for (int i = id_to_read_progress_[id] + 1; i <= page; ++i) {
+            // changing the status of previous pages
+            ++pages_to_read_count_[i];
+        }
+
+        id_to_read_progress_[id] = page; // recording the progress of a given user
     }
 
-    void Cheer() {
+    void Cheer(std::istream& input) {
         int id;
-        std::cin >> id;
-        if (id_to_read_progress_[id] == -1) {                                                           // no read pages for this user
-            std::cout << 0 << std::endl;                                                                // means 0 % readers read less
+        input >> id;
+        if (id_to_read_progress_[id] == -1) {   // no read pages for this user
+            std::cout << 0 << std::endl;        // means 0 % readers read less
             return;
         }
-        if (pages_to_read_count_[0] == 1) {                                                             // only 1 user
-            std::cout << 1 << std::endl;                                                                // means 100 % of readers read less
+        if (pages_to_read_count_[0] == 1) { // only 1 user
+            std::cout << 1 << std::endl;    // means 100 % of readers read less
             return;
         }
         std::cout
             << std::setprecision(6)
-            << static_cast <double> (pages_to_read_count_[0]                                            // count of users who read at least first page (i.e. all users)
-                - pages_to_read_count_[id_to_read_progress_[id]])                                       // minus count of users who read pages equal as user
-            / static_cast <double> (pages_to_read_count_[0] - 1)                                        // divide by count of users who read at least first page (i.e. all users), except the user
+            // count of users who read at least first page (i.e. all users)
+            << static_cast <double> (pages_to_read_count_[0]
+                // minus count of users who read pages equal as user
+                - pages_to_read_count_[id_to_read_progress_[id]])
+            // divide by count of users who read at least first page (i.e. all users), except the user
+            / static_cast <double> (pages_to_read_count_[0] - 1)
             << std::endl;
     }
 
 private:
-    std::vector <int> id_to_read_progress_;                                                             // (index + 1 == user id), (value == page which user read)
-    std::vector <int> pages_to_read_count_;                                                             // (index + 1 == page number), (value == number of users read this page)
+    // (index + 1 == user id), (value == page which user read)
+    std::vector <int> id_to_read_progress_;
+    // (index + 1 == page number), (value == number of users read this page)
+    std::vector <int> pages_to_read_count_;
 };
 
 int main() {
     BookManager book_manager;
-    book_manager.Proceed();
+    book_manager.Proceed(std::cin);
 
     return 0;
 }
